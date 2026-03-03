f462ae65224c68d772c9ac1e58781b1e7a7a3295
Boris Deniseno <boris.denisenko@mapbox.com>
2026-03-03 17:59:16 +0100

    Sprint 8 task 1

--- app/src/main/java/ru/yandex/practicum/sprint8koh38/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh38/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.sprint8koh38
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MyTag", "onCreate")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
